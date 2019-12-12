ad7491cf3a24ffe40730e7a91c08097ffbcc8920
rodrickcalvin <rodrickwamala@gmail.com>
2019-12-12 16:55:20 +0300

    Adds the login page route

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,20 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-import Home from "../views/Home.vue";
+import Index from "../views/LandingPage.vue";
+import Login from "../views/Login.vue";
 
 Vue.use(VueRouter);
 
 const routes = [
   {
     path: "/",
-    name: "home",
-    component: Home
+    name: "index",
+    component: Index
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: Login
   }
 ];
 
