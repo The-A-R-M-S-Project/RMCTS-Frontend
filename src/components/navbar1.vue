81be53b83bbfd165a3fe43256d5f92c3c1d257ea
rodrickcalvin <rodrickwamala@gmail.com>
2019-12-12 13:08:23 +0300

    Creates Navbar1 component

---
+++ src/components/navbar1.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <nav
+      class="navbar navbar-light navbar-expand-lg fixed-top bg-white clean-navbar"
+    >
+      <div class="container">
+        <a class="navbar-brand logo" href="#">RMCTS</a
+        ><button
+          data-toggle="collapse"
+          class="navbar-toggler"
+          data-target="#navcol-1"
+        >
+          <span class="sr-only">Toggle navigation</span
+          ><span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navcol-1">
+          <ul class="nav navbar-nav ml-auto">
+            <li class="nav-item" role="presentation">
+              <a class="nav-link" href="login.html">Login</a>
+            </li>
+          </ul>
+        </div>
+      </div>
+    </nav>
+  </div>
+</template>
+<script>
+export default {
+  name: "navbar1"
+};
+</script>
