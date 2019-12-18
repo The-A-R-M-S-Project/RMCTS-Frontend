4351a76244f697926369c0358333b407dba8a6b5
ben-wycliff <ben12wycliff@gmail.com>
2019-12-18 16:02:50 +0300

    Creates catalog view

---
+++ src/views/Catalog.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="view">
+    <!-- central navigation component -->
+    <Navbar />
+    <div class="m-2" style="border-top: solid white 85px;">
+      <h1 class="text-center">Catalog will go here</h1>
+    </div>
+  </div>
+</template>
+
+<style>
+.view {
+}
+</style>
+
+<script>
+import Navbar from "@/components/User/Navigation";
+export default {
+  name: "catalog",
+  components: {
+    Navbar
+  }
+};
+</script>
