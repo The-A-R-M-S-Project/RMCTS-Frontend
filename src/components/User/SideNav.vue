fec0cda6eec28e9e9bc72cec48595203d2fc92d5
ben-wycliff <ben12wycliff@gmail.com>
2019-12-18 15:51:44 +0300

    Modifies side nav

--- src/components/User/SideNav.vue
+++ src/components/User/SideNav.vue
@@ -1,59 +1,77 @@
 <template>
   <div class="sidenav">
-    <nav class="sidebar-nav">
-      <div class="card">
-        <img
-          src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtNIvsoEpZeocbdx9ocsYo9DO1zG4OfEf093dUn78Us553niinuw&s"
-          height="auto"
-          width="auto"
-          class="card-img-top img-fluid"
-          alt="profile picture"
-          style="boarder-radius: 30px;"
-        />
-        <div class="card-body card">
-          <h5 class="card-title text-center">username</h5>
-          <p class="card-text text-center">user@gmail.com</p>
-        </div>
+    <nav class="sidebar-nav fixed">
+      <div>
         <ul class="list-group list-group-flush">
+          <li class="list-group-item text-center card-header">
+            <img
+              src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtNIvsoEpZeocbdx9ocsYo9DO1zG4OfEf093dUn78Us553niinuw&s"
+              class="card-img-top img-fluid profile-image"
+              alt="profile picture"
+              style="border-radius: 100px;"
+              height=""
+            />
+            <h5 class="p-0">username</h5>
+            <p class="p-1">user@gmail.com</p>
+          </li>
           <router-link
             class="sidenav-button list-group-item"
-            :class="[
-              currentRoute.includes('user/profile') ? 'button-active' : ''
-            ]"
-            to="/"
+            to="/user/profile"
           >
-            <button class="btn btn-primary btn-lg btn-block ph-2" type="button">
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[currentRoute.includes('profile') ? 'button-active' : '']"
+              type="button"
+            >
               <div class="row">
-                <div class="col-3"><li class="fa fa-user"></li></div>
-                <div class="col-9 text-left">Profile</div>
+                <div class="col-3">
+                  <li class="fa fa-user"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Profile</span>
+                </div>
               </div>
             </button>
           </router-link>
           <router-link
             class="sidenav-button list-group-item"
-            :class="[
-              currentRoute.includes('user/equipment') ? 'button-active' : ''
-            ]"
-            to="/"
+            to="/user/equipment"
           >
-            <button class="btn btn-primary btn-lg btn-block ph-2" type="button">
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[
+                currentRoute.includes('user/equipment') ? 'button-active' : ''
+              ]"
+              type="button"
+            >
               <div class="row">
-                <div class="col-3"><li class="fa fa-server"></li></div>
-                <div class="col-9 text-left">Equipment</div>
+                <div class="col-3">
+                  <li class="fa fa-server"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Equipment</span>
+                </div>
               </div>
             </button>
           </router-link>
           <router-link
             class="sidenav-button list-group-item"
-            :class="[
-              currentRoute.includes('user/activity') ? 'button-active' : ''
-            ]"
-            to="/"
+            to="/user/activity"
           >
-            <button class="btn btn-primary btn-lg btn-block ph-2" type="button">
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[
+                currentRoute.includes('user/activity') ? 'button-active' : ''
+              ]"
+              type="button"
+            >
               <div class="row">
-                <div class="col-3"><li class="fa fa-clock-o"></li></div>
-                <div class="col-9 text-left">Activity</div>
+                <div class="col-3">
+                  <li class="fa fa-clock-o"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Activity</span>
+                </div>
               </div>
             </button>
           </router-link>
@@ -65,7 +83,7 @@
         class="btn btn-secondary btn-lg btn-block button ph-2"
         type="button"
       >
-        logout
+        <span style="font-size: smaller;">logout</span>
       </button>
     </router-link>
   </div>
@@ -73,10 +91,11 @@
 
 <style>
 .sidenav {
-  background-color: rgb(199, 255, 255);
-  height: 86vh;
+  background-color: rgb(9, 162, 255);
+  height: 83vh;
   border-radius: 4px;
   position: relative;
+  padding-top: 2px;
 }
 .button {
   position: absolute;
@@ -88,6 +107,7 @@
 }
 .logout-button {
   color: white;
+  font-size: 20pt;
 }
 .logout-button:hover {
   color: white;
@@ -105,7 +125,17 @@
   text-decoration: none;
 }
 .button-active {
-  color: orange;
+  color: yellow;
+}
+.button-active:hover {
+  color: yellow;
+}
+.profile-image {
+  height: 30vh;
+  width: auto;
+}
+.card-header {
+  background-color: rgb(9, 162, 255);
 }
 </style>
 
