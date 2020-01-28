52d11cc32859b248148a20d703b836e64e3c6879
ben-wycliff <ben12wycliff@gmail.com>
2020-01-28 20:10:31 +0300

    Adds equipment card to catalog

--- src/views/Catalog.vue
+++ src/views/Catalog.vue
@@ -3,8 +3,24 @@
     <!-- central navigation component -->
     <Navbar />
     <div class="m-2" style="border-top: solid white 85px;">
-      <Search />
-      <h1 class="text-center">Catalog will go here</h1>
+      <div class="p-4">
+        <Search />
+      </div>
+      <div class="container">
+        <div class="row">
+          <div
+            v-for="equipment in equipmentList"
+            :key="equipment._id"
+            class="col-md-4 p-2"
+          >
+            <Equipment
+              :title="equipment.title"
+              :location="equipment.location"
+              :url="equipment.imageURL"
+            />
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -17,11 +33,20 @@
 <script>
 import Navbar from "@/components/User/Navigation";
 import Search from "@/components/User/Search";
+import Equipment from "@/components/equipment";
+import equipmentList from "@/services/equipment-service.js";
+
 export default {
   name: "catalog",
   components: {
     Navbar,
-    Search
+    Search,
+    Equipment
+  },
+  data() {
+    return {
+      equipmentList
+    };
   }
 };
 </script>
