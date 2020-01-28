f9e4c1cbadb976475d8e9f7dac6c920de073c2ca
ben-wycliff <ben12wycliff@gmail.com>
2020-01-28 20:10:02 +0300

    Creates equipment dummy services

---
+++ src/services/equipment-service.js
@@ -0,0 +1,56 @@
+module.exports = [
+  {
+    _id: 1,
+    title: "Electronics work bench",
+    location: "Makerere, CEDAT, Old building",
+    description:
+      "equipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "https://sc02.alicdn.com/kf/HTB1JnwmayzxK1Rjy1zkq6yHrVXaN/Detall-CE-certification-Technician-electronics-workbench-for.jpg_350x350.jpg"
+  },
+  {
+    _id: 2,
+    title: "Graphics processing unit",
+    location: "Makerere, NetLabs!UG, CEDAT, New building",
+    description:
+      "equipment description Lorem ipsum dolor sit ametequipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa. consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "http://www.astro.wisc.edu/assets/featured_slides/feature_gpu_computing_1.png"
+  },
+  {
+    _id: 3,
+    title: "Computer labaratory",
+    location: "Makerere, CEDAT, New building",
+    description:
+      "equipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "https://yalatechhub.com/wp-content/uploads/2018/12/IT-colleges-in-nepal.jpg"
+  },
+  {
+    _id: 4,
+    title: "Computer labaratory",
+    location: "Makerere, CEDAT, New building",
+    description:
+      "equipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "https://yalatechhub.com/wp-content/uploads/2018/12/IT-colleges-in-nepal.jpg"
+  },
+  {
+    _id: 5,
+    title: "Computer labaratory",
+    location: "Makerere, CEDAT, New building",
+    description:
+      "equipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "https://yalatechhub.com/wp-content/uploads/2018/12/IT-colleges-in-nepal.jpg"
+  },
+  {
+    _id: 6,
+    title: "Computer labaratory",
+    location: "Makerere, CEDAT, New building",
+    description:
+      "equipment description Lorem ipsum dolor sit amet consectetur adipisicing elit. Veritatis maxime consectetur iure cumque debitis quaerat ratione exercitationem magnam, quod, at in! Beatae ut perspiciatis ipsam distinctio voluptatibus ullam molestiae ipsa.",
+    imageURL:
+      "https://yalatechhub.com/wp-content/uploads/2018/12/IT-colleges-in-nepal.jpg"
+  }
+];
