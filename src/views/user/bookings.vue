95d5b65081fcbc1484d980d2848b41a0c67ddc47
ben-wycliff <ben12wycliff@gmail.com>
2020-01-30 20:13:17 +0300

    Creates interface for no bookings case

---
+++ src/views/user/bookings.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="bookings">
+    <h1 style="text-align: center; background-color: grey; color: white">
+      Bookings
+    </h1>
+    <div v-if="Activity.length == 0" class="none">
+      <p style="font-size: 22px;">You currently have no bookings</p>
+      <i
+        class="fa fa-exclamation-circle icon"
+        aria-hidden="true"
+        style="font-size: 25vw;"
+      ></i>
+    </div>
+    <div v-else></div>
+  </div>
+</template>
+
+<style>
+.none {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  border: solid 2px rgb(9, 162, 255);
+  border-radius: 15px;
+  height: 72vh;
+  margin: 2vh;
+}
+.icon {
+  color: rgb(240, 135, 135);
+}
+</style>
+
+<script>
+export default {
+  name: "user-activity",
+  data() {
+    return {
+      Activity: []
+    };
+  }
+};
+</script>
