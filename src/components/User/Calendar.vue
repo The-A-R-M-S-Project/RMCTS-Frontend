220b9a499407f562966ef2ecabadfe60d8658b41
rodrickcalvin <rodrickwamala@gmail.com>
2020-02-20 18:27:09 +0300

    Adds full calendar properties

--- src/components/User/Calendar.vue
+++ src/components/User/Calendar.vue
@@ -1,9 +1,39 @@
 <template>
-  <div></div>
+  <div>
+    <div class="container card">
+      <FullCalendar
+        defaultView="dayGridMonth"
+        :plugins="calendarPlugins"
+        :events="reservations"
+      />
+    </div>
+  </div>
 </template>
 
-<style scoped></style>
+<style lang="scss" scoped>
+@import "~@fullcalendar/core/main.css";
+@import "~@fullcalendar/daygrid/main.css";
+</style>
 
 <script>
-export default {}
+import FullCalendar from "@fullcalendar/vue";
+import dayGridPlugin from "@fullcalendar/daygrid";
+import reservations from "@/services/reservations.js";
+
+export default {
+  components: {
+    FullCalendar
+  },
+  data() {
+    return {
+      calendarPlugins: [dayGridPlugin],
+      reservations
+    };
+  },
+  methods: {
+    handleDateClick(tap) {
+      alert(tap.date);
+    }
+  }
+};
 </script>
