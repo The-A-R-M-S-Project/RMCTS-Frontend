b3412f96a6daa0093b4e9bc09e6a82e3001cae33
ben-wycliff <ben12wycliff@gmail.com>
2020-03-08 19:49:25 +0300

    Adds Equpiment Equipment editing component

---
+++ src/components/User/EditEquipment.vue
@@ -0,0 +1,127 @@
+<template>
+  <div class="containr">
+    <div class="bg-light" style="width: 100%; height: 100%;">
+      <div class="row" style="background-color:  rgb(205, 216, 226)">
+        <div class="col text-center">
+          <form action="" style="">
+            <input
+              type="text"
+              class="form-control mt-3"
+              placeholder="title"
+              v-model="title"
+            />
+            <input
+              type="text"
+              class="form-control mt-3"
+              placeholder="location, e.g (kampala, Makerere University, CEDAT, old builiding, room 143)"
+              v-model="location"
+            />
+            <input
+              type="text"
+              class="form-control mt-3"
+              placeholder="Image URL"
+              v-model="imageURL"
+            />
+            <textarea
+              class="form-control mt-4"
+              id="exampleFormControlTextarea1"
+              rows="6"
+              placeholder="Description"
+              v-model="description"
+            ></textarea>
+            <button
+              type="submit"
+              class="btn btn-primary mb-2 mt-3"
+              style="width: 100px;"
+              data-dismiss="modal"
+              @click="handleSubmit"
+            >
+              update
+            </button>
+            <div class="loading"></div>
+          </form>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style lang="scss" scoped>
+.containr {
+  display: flex;
+  justify-content: center;
+  background-color: rgb(205, 216, 226);
+}
+form {
+  max-width: 800px;
+  background-color: rgb(205, 216, 226);
+  margin: auto;
+}
+.loading {
+  display: flex;
+  justify-content: center;
+
+  div {
+    width: 1rem;
+    height: 1rem;
+    margin: 2rem 0.3rem;
+    background: white;
+    border-radius: 50%;
+    animation: 0.9s bounce infinite alternate;
+
+    &:nth-child(2) {
+      animation-delay: 0.3s;
+    }
+
+    &:nth-child(3) {
+      animation-delay: 0.6s;
+    }
+  }
+}
+
+@keyframes bounce {
+  to {
+    opacity: 0.3;
+    transform: translate3d(0, -1rem, 0);
+  }
+}
+</style>
+
+<script>
+// import { mapActions } from "vuex";
+
+export default {
+  name: "add-equipment",
+  data() {
+    return {
+      //   item: {
+      //     title: "",
+      //     location: "",
+      //     imageURL: "",
+      //     description: ""
+      //   }
+    };
+  },
+  props: {
+    title: String,
+    description: String,
+    location: String,
+    imageURL: String,
+    _id: String
+  },
+  methods: {
+    // ...mapActions(["addEquipment"]),
+    // handleSubmit(e) {
+    //   e.preventDefault();
+    //   this.addEquipment(this.item).then(
+    //     (this.item = {
+    //       title: "",
+    //       location: "",
+    //       imageURL: "",
+    //       description: ""
+    //     })
+    //   );
+    // }
+  }
+};
+</script>
