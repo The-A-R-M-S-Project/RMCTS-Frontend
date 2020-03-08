5dfdf4d8ea64d658998d4ab3afce44b9f036dd6c
ben-wycliff <ben12wycliff@gmail.com>
2020-03-08 15:02:17 +0300

    Adds persistedState instance as a plugin to the store

--- src/store/index.js
+++ src/store/index.js
@@ -2,10 +2,12 @@ import Vue from "vue";
 import Vuex from "vuex";
 import auth from "./modules/auth";
 import equipment from "./modules/equipment";
+import createPersistedState from "vuex-persistedstate";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [createPersistedState()],
   modules: {
     auth,
     equipment
