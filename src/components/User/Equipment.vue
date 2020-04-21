bbdbe2747beabbd79034628bd38c7281ede456c1
ben-wycliff <ben12wycliff@gmail.com>
2020-04-21 15:10:49 +0300

    Customises scrollbar

--- src/components/User/Equipment.vue
+++ src/components/User/Equipment.vue
@@ -9,7 +9,7 @@
             style="width: 100%; height: 40vh; border-top-left-radius: 20px;  object-fit: cover;"
           />
         </div>
-        <div class="col-7">
+        <div class="col-7 desc">
           <div class="">
             <h5 class="card-title">{{ title }}</h5>
             <p class="text-justify pr-3 mytext">
@@ -101,6 +101,29 @@
 .mytext {
   overflow-wrap: break-word;
   word-wrap: break-word;
+}
+.desc {
+  overflow-y: scroll;
+  height: 40vh;
+}
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: rgba(0, 123, 255, 0.363);
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: rgb(2, 74, 150);
 }
 </style>
 
