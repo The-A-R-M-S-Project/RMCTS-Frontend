8b06012834ebab5273db01f49949fe890c5cbbdf
rodrickcalvin <rodrickwamala@gmail.com>
2020-05-20 00:59:36 +0300

    Adds inputs fields to add event form

--- src/components/User/addEventModal.vue
+++ src/components/User/addEventModal.vue
@@ -9,42 +9,113 @@
           <form>
             <div class="form-group">
               <label for="email">Event Name</label>
-              <input class="form-control item" type="text" />
+              <input v-model="title" class="form-control item" type="text" />
             </div>
+
+            <div class="form-group row ml-2">
+              <div class="form-row">
+                <label for="time">Start:</label>
+              </div>
+              <div class="col-md-3">
+                <input
+                  v-model="startHr"
+                  class="form-control"
+                  id="time"
+                  placeholder="Hr"
+                />
+              </div>
+              <div class="col-md-3">
+                <input
+                  v-model="startMin"
+                  class="form-control"
+                  id="time"
+                  placeholder="Mins"
+                />
+              </div>
+
+              <div class="col-md-3">
+                <select v-model="startPeriod" class="form-control" id="time">
+                  <option v-for="i in periods" :key="i">{{ i }}</option>
+                </select>
+              </div>
+            </div>
+
             <div class="form-row">
               <div class="form-group col-md-4">
                 <label for="day">Day</label>
-                <select class="form-control" id="day">
+                <select v-model="startDay" class="form-control" id="day">
                   <option v-for="i in days" :key="i">{{ i }}</option>
                 </select>
               </div>
               <div class="form-group col-md-4">
                 <label for="month">Month</label>
-                <select class="form-control" id="month">
+                <select v-model="startMonth" class="form-control" id="month">
                   <option v-for="i in months" :key="i">{{ i }}</option>
                 </select>
               </div>
               <div class="form-group col-md-4">
                 <label for="year">Year</label>
-                <input class="form-control" type="text" id="year" />
+                <input
+                  v-model="startYear"
+                  class="form-control"
+                  type="text"
+                  id="year"
+                />
               </div>
             </div>
-            <div class="form-row">
-              <div class="form-group col-md-2">
-                <label for="hour">Hour</label>
-                <input class="form-control" id="hour" />
+
+            <div class="form-group row ml-2">
+              <div class="form-row">
+                <label for="time">End: </label>
               </div>
-              <div class="form-group col-md-2">
-                <label for="password">Minutes</label>
-                <input class="form-control" type="password" id="password" />
+              <div class="col-md-3">
+                <input
+                  v-model="endHr"
+                  class="form-control"
+                  id="time"
+                  placeholder="Hr"
+                />
               </div>
-              <div class="form-group col-md-3">
-                <label for="periods"></label>
-                <select class="form-control" id="periods">
+              <div class="col-md-3">
+                <input
+                  v-model="endMin"
+                  class="form-control"
+                  id="time"
+                  placeholder="Mins"
+                />
+              </div>
+
+              <div class="col-md-3">
+                <select v-model="endPeriod" class="form-control" id="time">
                   <option v-for="i in periods" :key="i">{{ i }}</option>
                 </select>
               </div>
             </div>
+
+            <div class="form-row">
+              <div class="form-group col-md-4">
+                <label for="day">Day</label>
+                <select v-model="endDay" class="form-control" id="day">
+                  <option v-for="i in days" :key="i">{{ i }}</option>
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="month">Month</label>
+                <select v-model="endMonth" class="form-control" id="month">
+                  <option v-for="i in months" :key="i">{{ i }}</option>
+                </select>
+              </div>
+              <div class="form-group col-md-4">
+                <label for="year">Year</label>
+                <input
+                  v-model="endYear"
+                  class="form-control"
+                  type="text"
+                  id="year"
+                />
+              </div>
+            </div>
+
             <div class="form-group">
               <label for="eventDetails">Add Details:</label>
               <textarea
@@ -89,7 +160,19 @@ export default {
         "Nov",
         "Dec"
       ],
-      periods: ["AM", "PM"]
+      periods: ["AM", "PM"],
+      startHr: "",
+      startMin: "",
+      startPeriod: "",
+      startDay: "",
+      startMonth: "",
+      startYear: "",
+      endHr: "",
+      endMin: "",
+      endPeriod: "",
+      endDay: "",
+      endMonth: "",
+      endYear: ""
     };
   }
 };
