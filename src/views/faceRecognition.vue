539d2b7583371fba062997c231f9b35b5854e817
ben-wycliff <ben12wycliff@gmail.com>
2020-05-19 16:57:14 +0300

    Creates face recognition view

---
+++ src/views/faceRecognition.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="camera">
+    <video autoplay class="webcam"></video>
+    <!-- <input type="email"> -->
+    <button class="btn btn-primary">detect</button>
+  </div>
+</template>
+
+<style scoped></style>
+
+<script>
+export default {
+  name: "face-recognition",
+  data() {
+    return {};
+  }
+};
+</script>
