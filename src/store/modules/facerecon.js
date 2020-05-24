051cde7b7b8bb38cd0a96bf1d90bab3e4011a5e0
ben-wycliff <ben12wycliff@gmail.com>
2020-05-24 21:49:36 +0300

    adds remote api address

--- src/store/modules/facerecon.js
+++ src/store/modules/facerecon.js
@@ -1,7 +1,7 @@
 /* eslint-disable */
 import axios from 'axios'
 
-const api = "http://localhost:5000/detect"
+const api = "https://facerecon-api.herokuapp.com/detect"
 
 const state = {
     detectionComplete: false,
@@ -33,7 +33,7 @@ const actions = {
 
             commit("updateUser", res.data.user)
         } catch (err) { console.log(err) };
-    }
+    },
 };
 
 export default {
