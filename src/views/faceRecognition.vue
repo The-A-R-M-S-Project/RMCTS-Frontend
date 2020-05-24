27f658819cd4878e69e4b746b4e0d76f93afd9b1
ben-wycliff <ben12wycliff@gmail.com>
2020-05-24 13:47:07 +0300

    Styles webcam stream for different screen sizes

--- src/views/faceRecognition.vue
+++ src/views/faceRecognition.vue
@@ -1,41 +1,76 @@
 <template>
   <div class="camera d-flex align-items-center flex-column">
     <div>
-      <video autoplay class="webcam"></video>
+      <video autoplay id="v" class="webcam"></video>
     </div>
     <!-- <input type="email"> -->
-    <hr style="width: 100vw" />
+    <div>
+      <h4 style="color: white">Your face has been as {{ name }}</h4>
+    </div>
     <div>
       <button
         class="btn btn-primary"
-        style="height: 80px; width: 80px; border-radius: 40px"
+        style="height: 75px; width: 75px; border-radius: 40px"
+        @click="recognizeFace"
       >
         detect
       </button>
+    </div>
+    <div>
+      <canvas class="canvas" width="620" height="500" id="c"></canvas>
     </div>
   </div>
 </template>
 
 <style scoped>
-.camera {
-  width: 100vw;
-  height: 100vh;
-  margin-top: 2vh;
+@media screen and (min-width: 500px) {
+  .camera {
+    background-image: linear-gradient(black, grey);
+    padding-top: 10px;
+    height: 100vh;
+  }
+  .webcam {
+    width: auto;
+    height: auto;
+    background-color: rgb(26, 25, 25);
+    max-width: 600px;
+    border-radius: 5px;
+  }
+  .canvas {
+    z-index: -1;
+    position: fixed;
+    top: 13px;
+  }
 }
-.webcam {
-  width: auto;
-  height: auto;
-  background-color: rgb(26, 25, 25);
-  max-width: 700px;
-  border-radius: 5px;
+@media screen and (max-width: 499px) {
+  video {
+    height: auto;
+    width: 100vw;
+  }
+  .canvas {
+    height: 75vw;
+    width: 100vw;
+    z-index: -1;
+    position: fixed;
+    top: 20px;
+  }
+  .camera {
+    background-image: linear-gradient(black, grey);
+    padding-top: 20px;
+    height: 100vh;
+  }
 }
 </style>
 
 <script>
+/* eslint-disable */
+import axios from "axios";
 export default {
   name: "face-recognition",
   data() {
-    return {};
+    return {
+      name: "..."
+    };
   },
   methods: {
     initialise() {
@@ -49,6 +84,19 @@ export default {
           videoPlayer.play();
         });
       }
+    },
+    recognizeFace() {
+      const canvas = document.getElementById("c");
+      const video = document.getElementById("v");
+      canvas.getContext("2d").drawImage(video, 0, 0);
+      axios
+        .post("http://localhost:5000/detect", {
+          datauri: `${canvas.toDataURL("image/png")}`
+        })
+        .then(res => {
+          console.log(res);
+        })
+        .catch(err => console.log(err));
     }
   },
   beforeMount() {
