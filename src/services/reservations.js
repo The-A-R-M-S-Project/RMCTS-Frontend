3eb709af6d2c7e0ae7cb2f8ec70396fbf9f700b1
rodrickcalvin <rodrickwamala@gmail.com>
2020-05-25 19:55:29 +0300

    changes event object format

--- src/services/reservations.js
+++ src/services/reservations.js
@@ -1,4 +1,11 @@
 module.exports = [
-  // { title: "event 1", date: "2020-02-01" },
+  {
+    id: "000001",
+    title: "event 1",
+    start: "2020-05-21",
+    end: "2020-05-21"
+    // startTime: "10:30 AM",
+    // endTime: "12:20 AM"
+  }
   // { title: "event 2", date: "2020-02-22" }
 ];
