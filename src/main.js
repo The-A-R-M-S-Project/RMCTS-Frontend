75be784aa23db565dda4d040e936d409f1a4e7d4
rodrickcalvin <rodrickwamala@gmail.com>
2020-05-25 19:38:54 +0300

    removes Kalendar

--- src/main.js
+++ src/main.js
@@ -3,12 +3,12 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-// import modal from "vue-js-modal";
-import PortalVue from "portal-vue";
+import modal from "vue-js-modal";
+// import PortalVue from "portal-vue";
 
 Vue.config.productionTip = false;
-// Vue.use(modal, { dynamic: true });
-Vue.use(PortalVue);
+Vue.use(modal, { dynamic: true });
+// Vue.use(PortalVue);
 
 new Vue({
   router,
