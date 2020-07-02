fad6df15c147f486fee22a6015920d3c95eeadb4
ben-wycliff <benclifg@gmail.com>
2020-07-02 23:05:12 +0300

    Initializes individual profile view

---
+++ src/views/individual/IndividualProfile.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    This is the user profile This is the user profile This is the user profile
+    This is the user profile This is the user profile This is the user profile
+  </div>
+</template>
+
+<script>
+export default {
+  name: "individual-profile"
+};
+</script>
+
+<style lang="stylus" scoped></style>
