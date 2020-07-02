0a5bf03ec3b3bb1a2c43675cb010a4223d81bb3a
ben-wycliff <benclifg@gmail.com>
2020-07-02 23:05:42 +0300

    Adds individual catalog and profile routes

--- src/router/index.js
+++ src/router/index.js
@@ -25,219 +25,229 @@ import OrgForm1 from "../views/institute/OrgReg1";
 import OrgForm2 from "../views/institute/OrgReg2";
 import EquipmentReservations from "../views/user/EquipmentReservations";
 
+// individual
+import Individual from "../views/individual/Individual";
+import IndividualLogin from "../views/individual/IndividualLogin";
+import IndividualProfile from "../views/individual/IndividualProfile";
+import IndividualCatalog from "../views/individual/IndividualCatalog";
 Vue.use(VueRouter);
 
-const routes = [
-  {
-    path: "/",
-    name: "index",
-    component: Index,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/individual-login",
-    name: "login",
-    component: Login,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/institute-login",
-    name: "orglogin",
-    component: OrgLogin,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/institute-register/first-step",
-    name: "first-step",
-    component: OrgForm1,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/institute-register/last-step",
-    name: "last-step",
-    component: OrgForm2,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/register",
-    name: "register",
-    component: Register,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/catalog",
-    name: "catalog",
-    component: Catalog,
-    meta: {
-      requiresAuth: true
-    }
-  },
-  {
-    path: "/token-sent",
-    name: "token-sent",
-    component: TokenSent,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/signed-up",
-    name: "signed-up",
-    component: SignedUp,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/face-auth",
-    name: "face-recognition",
-    component: FaceRecognition,
-    meta: {
-      guest: true
-    }
-  },
-  {
-    path: "/user",
-    name: "user",
-    component: User,
-    meta: {
-      requiresAuth: true
+const routes = [{
+        path: "/",
+        name: "index",
+        component: Index,
+        meta: {
+            guest: true
+        }
     },
-
-    children: [
-      {
-        path: "profile",
-        name: "user-profile",
-        component: UserProfile,
+    {
+        path: "/individual-login",
+        name: "login",
+        component: Login,
         meta: {
-          requiresAuth: true
+            guest: true
         }
-      },
-      {
-        path: "bookings",
-        name: "user-bookings",
-        component: UserBookings,
-        meta: {
-          requiresAuth: true
-        }
-      }
-    ]
-  },
-  {
-    path: "/institute",
-    name: "institute",
-    component: Institute,
-    meta: {
-      requiresAuth: true
-      // guest: true
     },
-    children: [
-      {
-        path: "profile",
-        name: "institute-profile",
-        component: InstituteProfile,
+    {
+        path: "/institute-login",
+        name: "orglogin",
+        component: OrgLogin,
         meta: {
-          requiresAuth: true
-          // guest: true
+            guest: true
         }
-      },
-      {
-        path: "equipment",
-        name: "institute-equipment",
-        component: InstituteEquipment,
+    },
+    {
+        path: "/institute-register/first-step",
+        name: "first-step",
+        component: OrgForm1,
         meta: {
-          requiresAuth: true
-          // guest: true
+            guest: true
         }
-      },
-      {
-        path: "reservations",
-        name: "equipment-reservations",
-        component: EquipmentReservations,
+    },
+    {
+        path: "/institute-register/last-step",
+        name: "last-step",
+        component: OrgForm2,
         meta: {
-          requiresAuth: true
-          // guest: true
+            guest: true
         }
-      },
-      {
-        path: "catalog",
-        name: "catalog-view",
+    },
+    {
+        path: "/register",
+        name: "register",
+        component: Register,
+        meta: {
+            guest: true
+        }
+    },
+    {
+        path: "/catalog",
+        name: "catalog",
         component: Catalog,
         meta: {
-          requiresAuth: true
+            requiresAuth: true
         }
-      },
-      {
-        path: "bookings",
-        name: "equipment-bookings",
-        component: EquipmentBookings,
+    },
+    {
+        path: "/token-sent",
+        name: "token-sent",
+        component: TokenSent,
         meta: {
-          requiresAuth: true
+            guest: true
         }
-      },
-      {
-        path: "details/:id",
-        name: "details",
-        component: ItemDetails,
-        watch: {
-          // eslint-disable-next-line no-unused-vars
-          $route(to, from) {
-            // react to route changes...
-          }
+    },
+    {
+        path: "/signed-up",
+        name: "signed-up",
+        component: SignedUp,
+        meta: {
+            guest: true
+        }
+    },
+    {
+        path: "/face-auth",
+        name: "face-recognition",
+        component: FaceRecognition,
+        meta: {
+            guest: true
+        }
+    },
+    {
+        path: "/user",
+        name: "user",
+        component: User,
+        meta: {
+            requiresAuth: true
         },
+
+        children: [{
+                path: "profile",
+                name: "user-profile",
+                component: UserProfile,
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: "bookings",
+                name: "user-bookings",
+                component: UserBookings,
+                meta: {
+                    requiresAuth: true
+                }
+            }
+        ]
+    },
+    {
+        path: "/institute",
+        name: "institute",
+        component: Institute,
         meta: {
-          requiresAuth: true
-        }
-      },
-      {
-        path: "make-reservation",
-        name: "reservation",
-        component: Reservation,
-        meta: {
-          requiresAuth: true
-        }
-      }
-    ]
-  }
+            requiresAuth: true
+                // guest: true
+        },
+        children: [{
+                path: "profile",
+                name: "institute-profile",
+                component: InstituteProfile,
+                meta: {
+                    requiresAuth: true
+                        // guest: true
+                }
+            },
+            {
+                path: "equipment",
+                name: "institute-equipment",
+                component: InstituteEquipment,
+                meta: {
+                    requiresAuth: true
+                        // guest: true
+                }
+            },
+            {
+                path: "reservations",
+                name: "equipment-reservations",
+                component: EquipmentReservations,
+                meta: {
+                    requiresAuth: true
+                        // guest: true
+                }
+            },
+            {
+                path: "catalog",
+                name: "catalog-view",
+                component: Catalog,
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: "bookings",
+                name: "equipment-bookings",
+                component: EquipmentBookings,
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: "details/:id",
+                name: "details",
+                component: ItemDetails,
+                watch: {
+                    // eslint-disable-next-line no-unused-vars
+                    $route(to, from) {
+                        // react to route changes...
+                    }
+                },
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: "make-reservation",
+                name: "reservation",
+                component: Reservation,
+                meta: {
+                    requiresAuth: true
+                }
+            },
+            {
+                path: "catalog",
+                name: "individual-catalog",
+                component: IndividualCatalog,
+                meta: {
+
+                }
+            }
+        ]
+    }
 ];
 
 const router = new VueRouter({
-  mode: "history",
-  routes
+    mode: "history",
+    routes
 });
 
 router.beforeEach((to, from, next) => {
-  if (to.matched.some(record => record.meta.requiresAuth)) {
-    if (localStorage.getItem("jwt") == null) {
-      alert("Signup/login");
-      next({
-        name: "index"
-      });
+    if (to.matched.some(record => record.meta.requiresAuth)) {
+        if (localStorage.getItem("jwt") == null) {
+            alert("Signup/login");
+            next({
+                name: "index"
+            });
+        } else {
+            next();
+        }
+        next();
+    } else if (to.matched.some(record => record.meta.guest)) {
+        if (localStorage.getItem("jwt") == null) {
+            next();
+        } else {
+            next({ name: "institute-profile" });
+        }
+        next();
     } else {
-      next();
+        next();
     }
-    next();
-  } else if (to.matched.some(record => record.meta.guest)) {
-    if (localStorage.getItem("jwt") == null) {
-      next();
-    } else {
-      next({ name: "institute-profile" });
-    }
-    next();
-  } else {
-    next();
-  }
 });
 export default router;
