d283893a0e0cda009a2dfff41dedd69d94948751
rodrickcalvin <rodrickwamala@gmail.com>
2020-07-16 15:50:19 +0300

    creates organisation sidenav

---
+++ src/components/Institute/orgSideNav.vue
@@ -0,0 +1,171 @@
+<template>
+  <div class="sidenav">
+    <nav class="sidebar-nav fixed">
+      <div class="pt-3">
+        <ul class="list-group list-group-flush">
+          <li class="list-group-item text-center card-header">
+            <img
+              :src="
+                institute.profileImage ||
+                  'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQtNIvsoEpZeocbdx9ocsYo9DO1zG4OfEf093dUn78Us553niinuw&s'
+              "
+              class="card-img-top img-fluid profile-image"
+              alt="profile picture"
+              style="border-radius: 300px;"
+              height=""
+            />
+            <h5 class="p-0">{{ institute.name }}</h5>
+            <p class="p-1">{{ institute.email }}</p>
+          </li>
+          <router-link
+            class="sidenav-button list-group-item"
+            to="/user/profile"
+          >
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[currentRoute.includes('profile') ? 'button-active' : '']"
+              type="button"
+            >
+              <div class="row">
+                <div class="col-3">
+                  <li class="fa fa-user"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Profile</span>
+                </div>
+              </div>
+            </button>
+          </router-link>
+          <router-link
+            class="sidenav-button list-group-item"
+            to="/user/equipment"
+          >
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[
+                currentRoute.includes('user/equipment') ? 'button-active' : ''
+              ]"
+              type="button"
+            >
+              <div class="row">
+                <div class="col-3">
+                  <li class="fa fa-server"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Equipment</span>
+                </div>
+              </div>
+            </button>
+          </router-link>
+          <router-link
+            class="sidenav-button list-group-item"
+            to="/user/bookings"
+          >
+            <button
+              class="btn btn-primary btn-lg btn-block ph-2"
+              :class="[
+                currentRoute.includes('user/bookings') ? 'button-active' : ''
+              ]"
+              type="button"
+            >
+              <div class="row">
+                <div class="col-3">
+                  <li class="fa fa-clock-o"></li>
+                </div>
+                <div class="col-9 text-left">
+                  <span style="font-size: smaller;">Bookings</span>
+                </div>
+              </div>
+            </button>
+          </router-link>
+        </ul>
+      </div>
+    </nav>
+    <button
+      class="btn btn-secondary btn-lg btn-block button ph-2 logout-button"
+      type="submit"
+      @click="handleLogout"
+    >
+      <span style="font-size: smaller;">logout</span>
+    </button>
+  </div>
+</template>
+
+<style scoped>
+.sidenav {
+  background-color: rgb(9, 128, 255);
+  height: 100vh;
+  min-height: 600px;
+  border-radius: 4px;
+  position: relative;
+}
+.button {
+  position: absolute;
+  bottom: 5px;
+}
+.container {
+  margin: 0px;
+  padding: 0px;
+}
+.logout-button {
+  color: white;
+  font-size: 20pt;
+}
+.card {
+  background-color: rgb(9, 128, 255);
+  max-height: 40vh;
+  width: auto;
+}
+.list-group-item {
+  padding: 0px;
+}
+.sidenav-button:hover {
+  text-decoration: none;
+}
+.button-active {
+  color: yellow;
+}
+.button-active:hover {
+  color: yellow;
+}
+.profile-image {
+  height: 30vh;
+  width: auto;
+  object-fit: cover;
+}
+.card-header {
+  background-color: rgb(9, 160, 255);
+}
+button {
+  border-radius: 0px;
+}
+</style>
+
+<script>
+/* eslint-disable */
+import { mapActions } from 'vuex'
+export default {
+  name: "SideNav",
+  data(){
+    return {
+      user: {},
+      institute: {}
+    }
+  },
+  computed: {
+    currentRoute() {
+      return this.$route.path;
+    },
+  },
+  methods: {
+    ...mapActions(['logout']),
+    handleLogout(){
+      this.logout().then(() =>  this.$router.push({ name: "index" })).catch((err)=> console.log(err))
+    }
+  },
+  created(){
+    this.user = JSON.parse(localStorage.getItem("user"))
+    this.institute = JSON.parse(localStorage.getItem("institute"))
+  }
+};
+</script>
