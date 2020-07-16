8f6cef85b7729163634c098dfda63fc6346cac2f
rodrickcalvin <rodrickwamala@gmail.com>
2020-07-16 15:50:54 +0300

    Creates institute hamepage

---
+++ src/views/institute/institute.vue
@@ -0,0 +1,59 @@
+<template>
+  <div class="view">
+    <!-- central navigation component -->
+    <!-- <Navbar /> -->
+    <div style="min-width: 1000px;">
+      <div class="row">
+        <div class="col-2">
+          <SideNav />
+        </div>
+        <div class="col-10 landing pt-3">
+          <router-view></router-view>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<style scoped>
+.view {
+  overflow-y: hidden;
+  overflow-x: scroll;
+}
+.landing {
+  height: 100vh;
+  min-height: 600px;
+  overflow-y: scroll;
+}
+/* width */
+::-webkit-scrollbar {
+  width: 5px;
+}
+
+/* Track */
+::-webkit-scrollbar-track {
+  background: #f1f1f1;
+}
+
+/* Handle */
+::-webkit-scrollbar-thumb {
+  background: #888;
+}
+
+/* Handle on hover */
+::-webkit-scrollbar-thumb:hover {
+  background: #555;
+}
+</style>
+
+<script>
+// import Navbar from "@/components/User/Navigation";
+import SideNav from "@/components/institute/orgSideNav";
+export default {
+  name: "user",
+  components: {
+    // Navbar,
+    SideNav
+  }
+};
+</script>
