f5d5e761e38263ad6ac3dcdc385f9c82e5f86fed
ben-wycliff <benclifg@gmail.com>
2020-07-28 22:02:44 +0300

    Creates unverified account alert

---
+++ src/components/notVerified.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="alert-component d-flex justify-content-center">
+    <div
+      class="alert alert-danger d-flex align-items-center justify-content-center"
+      role="alert"
+    >
+      <p>
+        Your email has not been verified, click
+        <a style="color: blue; cursor: pointer;">here</a> to request
+        verification token resend
+      </p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "not-verified"
+};
+</script>
+
+<style scoped>
+.alert-component {
+  position: fixed;
+  width: 100%;
+  top: 10px;
+}
+</style>
