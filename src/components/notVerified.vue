f1baa74ad9a453216b2537acc6f885360764de9a
ben-wycliff <benclifg@gmail.com>
2020-07-29 11:15:24 +0300

    Creates unverified account alert component

--- src/components/notVerified.vue
+++ src/components/notVerified.vue
@@ -1,21 +1,66 @@
 <template>
-  <div class="alert-component d-flex justify-content-center">
-    <div
-      class="alert alert-danger d-flex align-items-center justify-content-center"
-      role="alert"
-    >
-      <p>
-        Your email has not been verified, click
-        <a style="color: blue; cursor: pointer;">here</a> to request
-        verification token resend
-      </p>
+  <div class="alert-component">
+    <div class="alert alert-success " role="alert">
+      <div class="d-flex align-items-center justify-content-center">
+        <form class="form-inline">
+          <p>Your email has not been verified, please enter email:</p>
+          <input
+            v-validate="'required|email'"
+            v-model="email"
+            :class="{ input: true, 'is-danger': errors.has('email') }"
+            name="email"
+            type="email"
+            id="email"
+            class="form-control item ml-1 mr-1"
+          />
+          <p>
+            and click
+            <a
+              @click.prevent="validateBeforeSubmit"
+              class="pl-1 pr-2"
+              style="color: blue; cursor: pointer;"
+              >here</a
+            >
+            to request verification token resend
+          </p>
+        </form>
+      </div>
+      <div
+        v-show="errors.has('email')"
+        class="help is-danger d-flex align-items-center justify-content-center"
+        id="msg"
+      >
+        {{ errors.first("email") }}
+      </div>
     </div>
   </div>
 </template>
 
 <script>
+/* eslint-disable */
 export default {
-  name: "not-verified"
+  name: "not-verified",
+  data() {
+    return {
+      email: "",
+    };
+  },
+  methods: {
+    handleRedirect(e) {
+      e.preventDefault();
+      console.log(this.email);
+    },
+    // method to validate fields before submission
+    validateBeforeSubmit(e) {
+      this.$validator.validateAll().then((result) => {
+        if (result) {
+          // es-lint-disable-next-line
+          this.handleRedirect(e);
+          return;
+        }
+      });
+    },
+  },
 };
 </script>
 
@@ -24,5 +69,8 @@ export default {
   position: fixed;
   width: 100%;
   top: 10px;
+}
+.email {
+  border-radius: 5px;
 }
 </style>
--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -4,24 +4,30 @@ import axios from "axios";
 // state
 const state = {
   loading: false,
-  failed: false
+  failed: false,
+  accountVerified: false,
 };
 
 // mutations
 const mutations = {
-  auth_request: state => {
-    state.loading = true
-    state.failed = false
+  auth_request: (state) => {
+    state.loading = true;
+    state.failed = false;
+    state.accountVerified=false;
   },
   auth_success: (state) => {
     state.loading = false;
     state.failed = false;
+    state.accountVerified = false;
   },
-  auth_error: state => {
-    state.loading = false
-    state.failed = true
+  auth_error: (state) => {
+    state.loading = false;
+    state.failed = true;
   },
-  signup_success: state => (state.signup = "success")
+  signup_success: (state) => (state.signup = "success"),
+  verification_error: (state) => {
+    state.accountVerified = true,
+    state.loading = false}
 };
 
 // actions
@@ -29,32 +35,46 @@ const actions = {
   login: async function({ commit }, data) {
     try {
       commit("auth_request");
-      let res = await axios.post("https://rmcts-api.herokuapp.com/admins/login", data);
+      let res = await axios.post(
+        "https://rmcts-api.herokuapp.com/admins/login",
+        data
+      );
+      console.log(res.body);
       localStorage.setItem("jwt", res.data.token);
       localStorage.setItem("user", JSON.stringify(res.data.admin));
       localStorage.setItem("institute", JSON.stringify(res.data.admin));
       commit("auth_success");
     } catch (err) {
-      commit("auth_error")
+      console.log(err.response.data.type, err.response.data.type == "not-verified")
+      if (err.response.data.type == "not-verified") {
+        commit("verification_error");
+        console.log(err.response.data.type)
+      } else {
+        // console.log(err.response.data.type);
+        commit("auth_error");
+      }
     }
   },
   logout: function({ commit }) {
     if (localStorage.getItem("jwt") != null) {
       axios
         .post(`https://rmcts-api.herokuapp.com/admins/me/logout`)
-        .then(res => {
+        .then((res) => {
           localStorage.removeItem("jwt");
           localStorage.removeItem("user");
-          window.localStorage.clear()
+          window.localStorage.clear();
         })
-        .catch(err => {
+        .catch((err) => {
           console.log(err.response.data.message);
         });
     }
   },
   signup: async function({ commit }, user) {
     try {
-      let res = await axios.post("https://rmcts-api.herokuapp.com/admins", user);
+      let res = await axios.post(
+        "https://rmcts-api.herokuapp.com/admins",
+        user
+      );
       console.log(res);
       commit("signup_success");
     } catch (err) {
@@ -64,7 +84,10 @@ const actions = {
   },
   orgSignup: async function({ commit }, institute) {
     try {
-      let res = await axios.post("https://rmcts-api.herokuapp.com/admins", institute);
+      let res = await axios.post(
+        "https://rmcts-api.herokuapp.com/admins",
+        institute
+      );
       console.log(res);
       commit("signup_success");
     } catch (err) {
@@ -72,20 +95,20 @@ const actions = {
       alert(err);
     }
   },
-
 };
 
 // getters
 const getters = {
-  user: state => state.user,
-  institute: state => state.institute,
-  loading: state => state.loading,
-  auth_failed: state => state.failed
+  user: (state) => state.user,
+  institute: (state) => state.institute,
+  loading: (state) => state.loading,
+  auth_failed: (state) => state.failed,
+  account_verified: (state) => state.accountVerified,
 };
 
 export default {
   state,
   mutations,
   actions,
-  getters
+  getters,
 };
