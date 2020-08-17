4e0d2830837dd61370f7c7b9c7b397ed1afc0300
ben-wycliff <benclifg@gmail.com>
2020-08-17 14:53:07 +0300

    Uses instance interceptors to handle expired session

--- src/main.js
+++ src/main.js
@@ -3,18 +3,50 @@ import App from "./App.vue";
 import "./registerServiceWorker";
 import router from "./router";
 import store from "./store";
-import axios from "axios";
 import VeeValidate from "vee-validate";
 import modal from "vue-js-modal";
+import api from "./api";
 // import PortalVue from "portal-vue";
 
-Vue.prototype.$http = axios;
 Vue.prototype.$user = JSON.parse(localStorage.getItem("user"));
-axios.interceptors.request.use(config => {
-  const token = localStorage.getItem("jwt");
-  config.headers.Authorization = "Bearer " + token;
-  return config;
-});
+Vue.prototype.$http = api;
+api.defaults.timeout = 10000;
+
+api.interceptors.request.use(
+  config => {
+    const token = localStorage.getItem("jwt");
+    if (token) {
+      config.headers.Authorization = "Bearer " + token;
+    }
+    return config;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
+
+api.interceptors.response.use(
+  response => {
+    if (response.status === 200 || response.status === 201) {
+      return Promise.resolve(response);
+    } else {
+      return Promise.reject(response);
+    }
+  },
+  error => {
+    if (error.response.status) {
+      switch (error.response.status) {
+        case 404:
+          // what to do;
+          break;
+        case 401:
+          alert("session expired");
+          router.push("/session-expired");
+          return Promise.reject(error.response);
+      }
+    }
+  }
+);
 Vue.config.productionTip = false;
 Vue.use(VeeValidate, {
   classes: true,
