2a68e911f5a5c5e07b081fa2644c371bdf0efc87
rodrickcalvin <rodrickwamala@gmail.com>
2020-08-24 01:42:33 +0300

    Updates institution edit user information

--- src/views/institute/OrgEditProfile.vue
+++ src/views/institute/OrgEditProfile.vue
@@ -2,40 +2,31 @@
   <div class="m-5 px-5">
     <form>
       <div class="form-group">
-        <label for="exampleTextarea1">Bio</label>
+        <label for="exampleTextarea1">Company Description</label>
         <textarea
           class="form-control"
           id="exampleTextarea1"
-          placeholder="Brief description of who you are"
+          placeholder="Brief description of company"
           rows="3"
           v-model="user.bio"
         ></textarea>
       </div>
       <div class="form-group">
-        <label for="institution">First Name</label>
+        <label for="institutionName">Username</label>
         <input
-          placeholder=""
+          placeholder="Company Name"
           class="form-control"
-          id="input-firstname"
-          v-model="user.firstname"
-        />
-      </div>
-      <div class="form-group">
-        <label for="input-title">Last Name</label>
-        <input
-          placeholder="e.g. Lecturer, Student"
-          class="form-control"
-          id="input-lastname"
-          v-model="user.lastname"
-        />
-      </div>
-      <div class="form-group">
-        <label for="input-department">Username</label>
-        <input
-          placeholder="e.g. Lecturer, Student"
-          class="form-control"
-          id="input-department"
+          id="institutionName"
           v-model="user.username"
+        />
+      </div>
+      <div class="form-group">
+        <label for="institutionWebsite">Company Website</label>
+        <input
+          placeholder="e.g.  www.companyname.com"
+          class="form-control"
+          id="institutionWebsite"
+          v-model="user.websiteURL"
         />
       </div>
       <div class="form-group">
@@ -78,47 +69,42 @@
 </template>
 
 <script>
-/* eslint-disable */
-
 export default {
-  name: "individual-editprofile",
+  name: "institution-editprofile",
   data() {
     return {
       contact: "",
-      user: {},
+      user: {}
     };
   },
   methods: {
     async handleSubmit(e) {
       e.preventDefault();
-     await this.$store.dispatch("updateProfile", {
-        firstname: this.user.firstname,
-        lastname: this.user.lastname,
+      await this.$store.dispatch("updateProfile", {
+        websiteURL: this.user.websiteURL,
         bio: this.user.bio,
-        contact: `${this.contact.length==9? `+256${this.contact}`:""}`,
+        contact: `${this.contact.length == 9 ? `+256${this.contact}` : ""}`,
         address: this.user.address,
         username: this.user.username
       });
-     await this.$store.dispatch("profile")
-     await this.$router.push({name: "individual-profile"})
+      await this.$store.dispatch("profile");
+      await this.$router.push({ name: "institution-profile" });
     },
     validateBeforeUpdate(e) {
-        this.$validator.validateAll().then(result => {
-          if (result) {
-            // es-lint-disable-next-line
-            this.handleSubmit(e)
-            return;
-          }
-        });
-      }
+      this.$validator.validateAll().then(result => {
+        if (result) {
+          this.handleSubmit(e);
+          return;
+        }
+      });
+    }
   },
   created() {
-    // this.$store.dispatch("login")
-    this.user = this.$store.getters.user_profile
+    this.user = this.$store.getters.user_profile;
     if (this.user.contact.length > 4) {
       this.contact = this.user.contact.substring(4);
     }
-  },
+  }
 };
 </script>
 
