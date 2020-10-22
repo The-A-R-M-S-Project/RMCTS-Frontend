49a3428b8be576b6a639f3c834f2f19a212fb698
ben-wycliff <benclifg@gmail.com>
2020-10-22 09:40:08 +0300

    Handles authentication error for institution

--- src/store/modules/auth.js
+++ src/store/modules/auth.js
@@ -47,6 +47,7 @@ const actions = {
     try {
       commit("auth_request");
       let res = await api.post("users/login", data);
+      console.log(res);
       const user = res.data.data.user;
       const token = res.data.token;
       if (user.role === "institution") {
@@ -57,9 +58,13 @@ const actions = {
         commit("auth_error");
       }
     } catch (err) {
-      if (err.data.type == "not-verified") {
-        commit("verification_error");
-      } else {
+      console.log("err", err);
+      try {
+        if (err.data.type == "not-verified") {
+          commit("verification_error");
+          return;
+        }
+      } catch (err) {
         commit("auth_error");
       }
     }
@@ -68,6 +73,7 @@ const actions = {
     try {
       commit("auth_request");
       let res = await api.post("users/login", data);
+      console.log("res ", res);
       const user = res.data.data.user;
       const token = res.data.token;
       if (user.role === "individual") {
@@ -78,9 +84,13 @@ const actions = {
         commit("auth_error");
       }
     } catch (err) {
-      if (err.data.type == "not-verified") {
-        commit("verification_error");
-      } else {
+      console.log("err", err);
+      try {
+        if (err.data.type == "not-verified") {
+          commit("verification_error");
+          return;
+        }
+      } catch (err) {
         commit("auth_error");
       }
     }
